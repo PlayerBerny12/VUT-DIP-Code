2de8fdcd5518378f163f8d3ef49ea5be3966adaa
PlayerBerny12 <ber.honza12@seznam.cz>
2023-04-22 12:37:36 +0200

    Controller, Entrypoint template updates, list of supported file types

--- server/processing_units/controller.py
+++ server/processing_units/controller.py
@@ -1,42 +1,69 @@
+from typing import Dict, Any
+
 import aiohttp
 import asyncio
+import json
 import os
 import pika
 
 PROCESSING_UNITS_URLS = [f"http://localhost:{port}/detect" for port in os.environ["ProcessingUnitsPorts"].split(":")]
 
-async def http_get_async(session, url):
-    async with session.get(url) as resp:
-        json = await resp.json()
-        return json
+async def http_get_async(session: Any, url: str, params: Dict[str, str]):
+    async with session.get(url, params=params) as resp:
+        if resp.status == 200:
+            json = await resp.json()
+            return json
+        else :
+            return {"error": "Internal error occurred."}
 
-async def call_all_processing_units(urls):
+async def call_all_processing_units(urls: str, params: Dict[str, str]):
     async with aiohttp.ClientSession() as session:
         tasks = []
         for url in urls:
-            tasks.append(asyncio.ensure_future(http_get_async(session, url)))
+            tasks.append(asyncio.ensure_future(http_get_async(session, url, params)))
 
         responses = await asyncio.gather(*tasks)
     return responses
 
-def on_message_received(channel, method, properties, body):
-    print(f"Received body: {body}")
-    
-    data = asyncio.run(call_all_processing_units(PROCESSING_UNITS_URLS))
-    print(data)    
-
-    channel.basic_ack(delivery_tag=method.delivery_tag)    
-
-if __name__ == "__main__":
+def create_channel(queue_name: str):
     connection_parameters = pika.ConnectionParameters(os.environ["RabbitMQ"])
     connection = pika.BlockingConnection(connection_parameters)
-    queue = os.environ["RabbitMQQueue"]    
 
     channel = connection.channel()
-    channel.queue_declare(queue=queue)
+    channel.queue_declare(queue=queue_name)   
+
+    return channel
+
+def on_message_received(channel: Any, method: Any, _, body: bytes):
+    params = body.decode("utf8").replace("\'", "\"")    
+    params = json.loads(params)
+    params_lowecase = {key.lower():value for key, value in params.items()}
+    print(f"Received body: {params_lowecase}")    
+
+    responses = asyncio.run(call_all_processing_units(PROCESSING_UNITS_URLS, params_lowecase))
+    # import requests
+    # responses = []
+    # for url in PROCESSING_UNITS_URLS:
+    #     respons = requests.get(url, params=params_lowecase)
+    #     responses.append(respons.json())
+
+    print(f"Received responses: {responses}")
+
+    queue_output = os.environ["RabbitMQOutputQueue"]    
+    channel_output = create_channel(queue_output)
+    channel_output.basic_publish("", queue_output, str(responses))
+    
+    channel.basic_ack(delivery_tag=method.delivery_tag)    
+
+def main():
+    queue = os.environ["RabbitMQQueue"]
+    channel = create_channel(queue)
+            
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue=queue, on_message_callback=on_message_received)
 
     print(f"Start consuming: {queue}")
-    print(PROCESSING_UNITS_URLS)
     channel.start_consuming()
+
+if __name__ == "__main__":
+    main()
