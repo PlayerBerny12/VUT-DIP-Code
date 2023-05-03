d176e15d4022ce041752ec003a5f01b4caa9feb4
PlayerBerny12 <ber.honza12@seznam.cz>
2023-05-03 23:28:29 +0200

    Tests fix

--- server/tests/test_case1.py
+++ server/tests/test_case1.py
@@ -1,7 +1,8 @@
 from argparse import ArgumentParser
 from os import path, listdir
 from time import time
-from .test_core import send_request, get_responses, map_responses
+from test_core import send_request, get_responses, map_responses
+
 
 def parse_args():
     parser = ArgumentParser()
@@ -10,38 +11,44 @@ def parse_args():
         "--real_dir",
         "--real",
         help="Directory containing real data.",
-        type=str,        
+        type=str,
+        required=True
     )
     parser.add_argument(
         "--fake_dir",
         "--fake",
         help="Directory containing fake data.",
         type=str,
+        required=True
     )
-      
+
     args = parser.parse_args()
     return args
 
+
 def processFile(filename, directory, file_type, output_file):
     file_path = path.join(directory, filename)
-    
+
     if path.isfile(file_path):
         start_time = time()
         requestID = send_request(filename, file_path)
         responses = get_responses(requestID)
         end_time = time()
 
-        output_file.write(f"{file_type};{map_responses(responses)};{path.getsize(file_path)};{end_time-start_time}\n")      
+        output_file.write(
+            f"{file_type};{map_responses(responses)};{path.getsize(file_path)};{end_time-start_time}\n")
+
 
 def main():
     args = parse_args()
-    
+
     with open("output.csv", "a") as output:
         for filename in listdir(args.real_dir):
             processFile(filename, args.real_dir, 1, output)
 
         for filename in listdir(args.fake_dir):
             processFile(filename, args.fake_dir, 0, output)
+
 
 if __name__ == "__main__":
     main()
--- server/tests/test_core.py
+++ server/tests/test_core.py
@@ -2,35 +2,35 @@ import requests
 
 from time import sleep
 
-DFDF_ENDPOINT = "http://localhost"
+DFDF_ENDPOINT = "http://20.4.98.50"
+
 
 def send_request(filename, file_path):
-    headers = {
-        "Content-Type": "multipart/form-data"
-    }
     file = {
-        "file": (filename, open(file_path, "rb")),
+        "file": (filename, open(file_path, "rb"), "audio/wav"),
     }
     
-    response = requests.post(f"{DFDF_ENDPOINT}/api/detect/file", headers=headers, files=file)
+    response = requests.post(f"{DFDF_ENDPOINT}/api/detect/file", files=file)
     return int(response.text)
+
 
 def get_responses(request_ID):
     while True:
-        response = requests.get(f"{DFDF_ENDPOINT}/api/request/results", { "requestID": request_ID})
+        response = requests.get(
+            f"{DFDF_ENDPOINT}/api/request/results", {"requestID": request_ID})
 
         if response.status_code == 200:
             break
-        
+
         sleep(0.25)
-    
+
     return response.json()
 
-def map_responses(responses):
-    stringBuilder = f"{responses.value}"
 
-    for response in responses.responses:
-        stringBuilder += f";{response}"
+def map_responses(responses):
+    stringBuilder = f"{responses['value']}"
+
+    for response in responses["responses"]:
+        stringBuilder += f";{response['value']}"
 
     return stringBuilder
-    
